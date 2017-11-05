ea0c9c73c38aeeddf694fab22f3b496b03664550
liangxinwei <sivan.shanghai@gmail.com>
2017-11-05 20:02:20 +0800

    update

--- README.md
+++ README.md
@@ -1,19 +1,11 @@
 # 仿饿了么 app
 
-## 使用的技术:
-* 接入了 react-redux store
-* 接入了 react-router，并通过中间件注入到 store 中，统一通过触发 action 实现路由跳转
-* 接入了 react-i18next 实现国际化
-* 分离 action、reducer、middleware 到各模块中，最后合并并注入到 store 中
-* 使用 axios 请求数据
-
-## 启动
-```
-npm install -g cnpm --registry=https://registry.npm.taobao.org // 全局安装 cnpm，已安装请跳过
-cnpm i
-npm run lintfix // 默认换行符是 LF，如果被 git 或 编辑器自动换成 CRLF，则运行
-npm start
-```
+## 使用的主要类库:
+* 管理数据　　react-redux
+* 路由跳转　　react-router, react-router-redux
+* 国际化　　　react-i18next
+* UI　组件　　ant-mobile
+* 请求数据　　axios
 
 ## 注意
 git 设置下面两个，否则会默认替换换行符为系统的换行符。此工程默认使用 Unix 系统换行符 LF。
@@ -22,8 +14,16 @@ git config core.autocrlf false //关闭 git 换行符的自动转换功能
 git config core.safecrlf true  //打开 git 提交换行符混用校验功能
 ```
 
+## 启动
+```
+npm install -g cnpm --registry=https://registry.npm.taobao.org // 全局安装 cnpm，已安装请跳过
+cnpm i
+npm run lintfix // 本工程默认换行符是 LF，如果被 git 或 IDE 自动换成 CRLF，则运行
+npm start
+```
+
 ## 编辑器支持 IDEA
-Setting -> Languages & Frameworks -> JavaScript -> 修改为 **ECMAScript 6**
-    
+Setting -> Languages -> JavaScript -> 修改为 **ECMAScript 6**
+
 ## 后台仓库地址
 [node-ele](https://github.com/liangxinwei/node-ele)
--- src/app/components/me/OwnerPage.jsx
+++ src/app/components/me/OwnerPage.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {translate} from 'react-i18next';
-import {Toast} from 'antd-mobile';
+import {createForm} from 'rc-form';
+import {Toast, Picker, List, WhiteSpace} from 'antd-mobile';
 
 import Constants from '../../constants';
 import {util, axiosInstance, connectToStore} from '../../utils';
@@ -12,6 +13,7 @@ const {ENTER_BUSINESS, GOTO} = Constants;
 /**
  * Header View
  */
+@createForm()
 @translate(['me'], {wait: true})
 @connectToStore
 class OwnerPage extends Component {
@@ -19,16 +21,212 @@ class OwnerPage extends Component {
         super(props);
     }
 
+    state = {
+        data: [],
+        cols: 1,
+        pickerValue: [],
+        asyncValue: [],
+        sValue: ['2013', '春'],
+        visible: false
+    };
+
+    onPickerChange = (val) => {
+        console.log(val);
+        let colNum = 1;
+        const d = [...this.state.data];
+        const asyncValue = [...val];
+        if (val[0] === '11') {
+            d.forEach((i) => {
+                if (i.value === '11') {
+                    colNum = 2;
+                    if (!i.children) {
+                        i.children = [{
+                            value: 'bj-east',
+                            label: '东城区'
+                        }, {
+                            value: 'zj-west',
+                            label: '西城区'
+                        }];
+                        asyncValue.push('zj-nb');
+                    } else if (val[1] === 'zj-west') {
+                        i.children.forEach((j) => {
+                            if (j.value === 'zj-west') {
+                                j.children = [{
+                                    value: 'zj-hz-xh',
+                                    label: '西湖区'
+                                }];
+                                asyncValue.push('zj-hz-xh');
+                            }
+                        });
+                        colNum = 3;
+                    }
+                }
+            });
+        } else {
+            colNum = 1;
+        }
+        this.setState({
+            data: d,
+            cols: colNum,
+            asyncValue
+        });
+    };
+
+    onClick = () => {
+        setTimeout(() => {
+            this.setState({
+                data: [
+                    {
+                        value: '11',
+                        label: '北京市',
+                        spell: 'BeiJingShi'
+                    }, {
+                        value: '12',
+                        label: '天津市',
+                        spell: 'TianJinShi'
+                    }, {
+                        value: '13',
+                        label: '河北省',
+                        spell: 'HeBeiSheng'
+                    }, {
+                        value: '14',
+                        label: '山西省',
+                        spell: 'ShanXiSheng'
+                    }, {
+                        value: '15',
+                        label: '内蒙古自治区',
+                        spell: 'NeiMengGuZiZhiQu'
+                    }, {
+                        value: '21',
+                        label: '辽宁省',
+                        spell: 'LiaoNingSheng'
+                    }, {
+                        value: '22',
+                        label: '吉林省',
+                        spell: 'JiLinSheng'
+                    }, {
+                        value: '23',
+                        label: '黑龙江省',
+                        spell: 'HeiLongJiangSheng'
+                    }, {
+                        value: '31',
+                        label: '上海市',
+                        spell: 'ShangHaiShi'
+                    }, {
+                        value: '32',
+                        label: '江苏省',
+                        spell: 'JiangSuSheng'
+                    }, {
+                        value: '33',
+                        label: '浙江省',
+                        spell: 'ZheJiangSheng'
+                    }, {
+                        value: '34',
+                        label: '安徽省',
+                        spell: 'AnHuiSheng'
+                    }, {
+                        value: '35',
+                        label: '福建省',
+                        spell: 'FuJianSheng'
+                    }, {
+                        value: '36',
+                        label: '江西省',
+                        spell: 'JiangXiSheng'
+                    }, {
+                        value: '37',
+                        label: '山东省',
+                        spell: 'ShanDongSheng'
+                    }, {
+                        value: '41',
+                        label: '河南省',
+                        spell: 'HeNanSheng'
+                    }, {
+                        value: '42',
+                        label: '湖北省',
+                        spell: 'HuBeiSheng'
+                    }, {
+                        value: '43',
+                        label: '湖南省',
+                        spell: 'HuNanSheng'
+                    }, {
+                        value: '44',
+                        label: '广东省',
+                        spell: 'GuangDongSheng'
+                    }, {
+                        value: '45',
+                        label: '广西壮族自治区',
+                        spell: 'GuangXiZhuangZuZiZhiQu'
+                    }, {
+                        value: '46',
+                        label: '海南省',
+                        spell: 'HaiNanSheng'
+                    }, {
+                        value: '50',
+                        label: '重庆市',
+                        spell: 'ChongQingShi'
+                    }, {
+                        value: '51',
+                        label: '四川省',
+                        spell: 'SiChuanSheng'
+                    }, {
+                        value: '52',
+                        label: '贵州省',
+                        spell: 'GuiZhouSheng'
+                    }, {
+                        value: '53',
+                        label: '云南省',
+                        spell: 'YunNanSheng'
+                    }, {
+                        value: '54',
+                        label: '西藏自治区',
+                        spell: 'XiCangZiZhiQu'
+                    }, {
+                        value: '61',
+                        label: '陕西省',
+                        spell: 'ShanXiSheng'
+                    }, {
+                        value: '62',
+                        label: '甘肃省',
+                        spell: 'GanSuSheng'
+                    }, {
+                        value: '63',
+                        label: '青海省',
+                        spell: 'QingHaiSheng'
+                    }, {
+                        value: '64',
+                        label: '宁夏回族自治区',
+                        spell: 'NingXiaHuiZuZiZhiQu'
+                    }, {
+                        value: '65',
+                        label: '新疆维吾尔自治区',
+                        spell: 'XinJiangWeiWuErZiZhiQu'
+                    }
+                ]
+            });
+        }, 120);
+    }
+
     componentDidMount() {
     }
 
     render() {
         let self = this;
-        const {t} = this.props;
+        const {t, form} = self.props;
+        const {getFieldProps} = form;
         return (
             <div className='app-me'>
                 <Header title='我的页面'/>
-                <h1>我的页面</h1>
+                <WhiteSpace size="lg"/>
+                <Picker
+                    extra="更改"
+                    data={this.state.data}
+                    cols={this.state.cols}
+                    value={this.state.asyncValue}
+                    onPickerChange={this.onPickerChange}
+                    onOk={v => console.log(v)}
+                >
+                    <button onClick={this.onClick}>123</button>
+                </Picker>
             </div>
         );
     }
